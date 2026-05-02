eeed69ffea08cf030de94adc434d9fa189742cf1
usershokhrukh <asxurovsit@gmail.com>
2026-05-02 19:59:33 +0500

    UI-style fulled

--- src/components/UsersRender.jsx
+++ src/components/UsersRender.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 
 const UsersRender = ({users}) => {
-  console.log(users);
   return (
     users?.map(item => (
-      <div>
-        <p>name: {item?.name}</p>
-        <p>email: {item?.email}</p>
-        <p>phone: {item?.phone}</p>
-        <p>username: {item?.username}</p>
-        <p>website: {item?.website}</p>
-        <p>city: {item?.address?.city}</p>
+      <div className='main__items'>
+        <p className='main__text'><span className='main__secondary-text'>name:</span> {item?.name}</p>
+        <p className='main__text'><span className='main__secondary-text'>email:</span> {item?.email}</p>
+        <p className='main__text'><span className='main__secondary-text'>phone:</span> {item?.phone}</p>
+        <p className='main__text'><span className='main__secondary-text'>username:</span> {item?.username}</p>
+        <p className='main__text'><span className='main__secondary-text'>website:</span> {item?.website}</p>
+        <p className='main__text'><span className='main__secondary-text'>city:</span> {item?.address?.city}</p>
       </div>
     ))
   )
